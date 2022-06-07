49c1a4b32662c3484d17c0da4c33c513949a09f0
tcut-sci-tech <88429838+tcut-sci-tech@users.noreply.github.com>
2022-06-07 17:59:41 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-	$('.slider').slick({
-		autoplay: true,//自動的に動き出すか。初期値はfalse。
-		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-		speed: 500,//スライドのスピード。初期値は300。
-		slidesToShow: 3,//スライドを画面に3枚見せる
-		slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-		centerMode: true,//要素を中央ぞろえにする
-		variableWidth: true,//幅の違う画像の高さを揃えて表示
-		dots: true,//下部ドットナビゲーションの表示
-	});
+$(function(){
+  $(".btn-gnavi").on("click", function(){
+    var rightVal = 0;
+    if($(this).hasClass("open")) {
+      rightVal = -300;
+      $(this).removeClass("open");
+    } else {
+      $(this).addClass("open");
+    }
+    
+    $("#global-navi").stop().animate({
+      right: rightVal
+    }, 200);
+  });
+});
