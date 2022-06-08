cc3d578f63cf202a1f5445d06325a6c810619f55
tcut-sci-tech <88429838+tcut-sci-tech@users.noreply.github.com>
2022-06-08 16:56:49 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -113,43 +113,6 @@ $('#slider').vegas({
         timer:false,//プログレスバー非表示
 	});
 
-/*===========================================================*/
-/*印象編 4-13 ランダムに現れる（CSS x jQuery）*/
-/*===========================================================*/
-
-// 動きのきっかけの起点となるアニメーションの名前を定義
-function moveAnimation(){	
-//スクロールしたらランダムに出現	
-	var randomElm2 = $(".randomScroll");								//親要素取得
-	var randomElm2Child = $(randomElm2).children();					//親の子要素を取得
-	randomScrollAnime();
-	function randomScrollAnime(){
-		var elemPos = $(".randomScroll").offset().top-50;//要素より、50px上の
-		var scroll = $(window).scrollTop();
-		var windowHeight = $(window).height();
-		if (scroll >= elemPos - windowHeight){
-			if(randomElm2Child.length >0 ){ 									//配列数以上であれば処理をおこなう
-				var rnd = Math.floor(Math.random() * randomElm2Child.length); //配列数から表示する数値をランダムで取得
-				var moveData = "flipDown";//アニメーション名＝CSSのクラス名を指定
-				if(animeFlag){ 											//スクロールする度に動作するのでアニメーションが終わるまで処理をさせないようにする
-					animeFlag = false; 									//アニメーション処理が終わるまで一時的にfalseにする
-					$(randomElm2Child[rnd]).addClass(moveData); 				//アニメーションのクラスを追加
-					setTimeout(function(){
-						animeFlag = true; 								//次の処理をおこなうためにtrueに変更
-						randomScrollAnime(); 							//自身の処理を繰り返す
-					},200);												//0.2秒間隔で。※ランダムのスピード調整はこの数字を変更させる
-					randomElm2Child.splice(rnd,1); 							//アニメーション追加となった要素を配列から削除
-				}
-			}
-			
-		}else{
-			animeFlag = true;
-		}
-		
-	}
-}
-
-	var animeFlag = true;//スクロールする度に動作するのでアニメーションが終わるまで処理をさせないようにするための定義
 
 /*===========================================================*/
 /* 印象編 4 最低限おぼえておきたい動き*/
